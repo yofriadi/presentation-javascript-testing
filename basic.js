d5f519e3f86c78f39e4ff5f39066cf6381238111
Yofriadi Yahya <yofriadiyahya@gmail.com>
2018-11-06 13:52:41 +0700

    Add build command

--- basic.js
+++ basic.js
@@ -1,9 +1,9 @@
-describe('group tests together', () => {
+describe.only('group tests together', () => {
   test('something', () => {
     expect(something).toBe(expected);
   });
  
-  it('does something', () => {
+  it.only('does something', () => {
     expect(something).toBe(expected);
   });
 });
